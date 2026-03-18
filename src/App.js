bcd40e3eafcacfea8245c5d1057887423f08442e
musCodes1 <mustaphatafzi1@gmail.com>
2026-03-18 07:04:43 +0100

    component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header"
+import Post from './Post';
+import SideMenu from './SideMenu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <div style={{display:"flex", justifyContent:"center"}}>
+{/* POSTS AND SIDE MENU CONTAINER */}
+      <div style={{
+        display:"flex",
+        width:"70%"
+      
+      }}>
+
+      {/* POST CONTAINER */}
+      <div style={{ width : "70%"}}>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      </div>
+      {/* POST CONAINER=== */}
+
+      {/* Side menu */}
+      <div style={{width:"30%"}}>
+        <SideMenu/>
+      </div>
+      {/* Side menu ==== */}
+
+      </div>
+    {/* POSTS AND SIDE MENU CONTAINER */}
+      </div>
+      
+
     </div>
   );
 }
---
+++ src/Post.js
@@ -0,0 +1,15 @@
+export default function Post() {
+    return ( 
+        <div style = {{
+         padding:"10px", 
+         border:"Solid teal 5px", 
+         margin: "25px"
+        
+        }}>
+         <h2>This is the post title</h2>
+         <hr></hr>
+         <p>This is the post body</p>
+        </div>
+    )
+
+}
